2f36a8c93175dfc875df86adbc89ed5c6a73b305
jo3l17 <47827494+jo3l17@users.noreply.github.com>
2020-12-05 01:18:17 -0500

    last commit i believe

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/weather-app'));
+app.use(express.static('./dist/weatherApp'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/weather-app/index.html'));
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/wdd330-weatherapp/index.html'));
+// });
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: 'dist/weatherApp/' }
+    );
 });
 
 // Start the app by listening on the default Heroku port
